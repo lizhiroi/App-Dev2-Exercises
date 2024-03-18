fbfd1f5ecc3da175ec3c89638bb01308f5da4e38
ZHI LI <YUCUI_CHENLI@HOTMAIL.COM>
2024-03-18 09:09:37 -0400

    log4net

---
+++ Log4NetDemo/Log4NetDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4NetDemo
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                log.Info("Hello world");
+            }
+            catch(Exception e)
+            {
+                log.Error("Hello world .. "+e.Message);
+            }
+          
+        }
+    }
+}
